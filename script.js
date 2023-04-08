6f35cc63215063edcf8fe10a3663ed99ca5195da
felipe <felipecassianobarbosa@hotmail.com>
2023-04-08 04:15:32 -0300

    optmize script

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 const glossaryDiv = document.getElementById("glossary-div");
 const inputSearch = document.querySelector("input[type='search']");
 const image = document.getElementById("logo");
-image.onclick = () => {
-    
-    if (image.src == (document.URL+'header.png')) {
-        image.src = 'img/banner.png';
-    }
-    else {
-        image.src = 'img/header.png'
-    }
-}
+
 import glossary from './glossary.json' assert {type: 'json'}
-const words = glossary.words;
-words.sort(compare);
-function compare(a, b) {
+const words = glossary.words.sort(function(a, b) {
     if (a.englishWord > b.englishWord) {
         return 1;
     }
@@ -21,21 +11,40 @@ function compare(a, b) {
         return -1;
     }
     return 0;
+});
+
+inputSearch.oninput = () => {
+    glossaryDiv.innerHTML = "";
+    words.filter((item) => item.englishWord.toLowerCase().includes(inputSearch.value.toLowerCase())).forEach((element) => { createElements(element) });
 }
+
+document.body.onload = () => {
+    image.src = 'img/header.png';
+    words.forEach((element) => {
+        createElements(element)
+    });
+}
+
+image.onclick = () => {
+    if (image.src == (document.URL+'img/header.png')) {
+        image.src = 'img/banner.png';
+    }
+    else {
+        image.src = 'img/header.png'
+    }
+}
+
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
 function createElements(object) {
     const innerDiv = document.createElement('div');
     innerDiv.id = "term";
     const english = document.createElement('h2');
     const portuguese = document.createElement('h4');
     const description = document.createElement('p');
-    /*
-    .textContent(object.englishWord);
-    .textContent(object.portugueseWord);
-    .textContent(object.description);
-    */
+
     english.innerText = object.englishWord;
     portuguese.innerText = object.portugueseWord;
     if (object.description == "") {
@@ -54,14 +63,4 @@ function createElements(object) {
         innerDiv.appendChild(singularity);
     }
     glossaryDiv.appendChild(innerDiv);
-}
-inputSearch.oninput = () => {
-    glossaryDiv.innerHTML = "";
-    words.filter((item) => item.englishWord.toLowerCase().includes(inputSearch.value.toLowerCase())).forEach((element) => { createElements(element) });
-}
-document.body.onload = () => {
-    image.src = 'img/header.png';
-    words.forEach((element) => {
-        createElements(element)
-    });
 }
