174a276e13d317d141e58b6fa1f52dd50af087c0
felipe <felipecassianobarbosa@hotmail.com>
2023-04-09 04:41:31 -0300

    optimize search, make filter buttons responsive

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const glossaryDiv = document.getElementById("glossary-div");
 const inputSearch = document.querySelector("input[type='search']");
 const image = document.getElementById("logo");
+const buttonDiv = document.getElementById("filter-buttons");
+const redefineButton = document.getElementById("redefine-button");
+const backToTopButton = document.getElementById('back-to-top');
 
 import glossary from './glossary.json' assert {type: 'json'}
-const words = glossary.words.sort(function(a, b) {
+import colors from './colors.json' assert {type: 'json'}
+const singularities = [];
+glossary.words.forEach((item) => {
+    if (!singularities.includes(item.type)) {
+        singularities.push(item.type);
+    }
+})
+
+const words = glossary.words.sort(function (a, b) {
     if (a.englishWord > b.englishWord) {
         return 1;
     }
@@ -13,25 +24,51 @@ const words = glossary.words.sort(function(a, b) {
     return 0;
 });
 
-inputSearch.oninput = () => {
-    glossaryDiv.innerHTML = "";
-    words.filter((item) => item.englishWord.toLowerCase().includes(inputSearch.value.toLowerCase())).forEach((element) => { createElements(element) });
-}
+let clickedButtons = [];
 
 document.body.onload = () => {
     image.src = 'img/header.png';
     words.forEach((element) => {
+        if (!singularities.includes(element.type)) {
+            singularities.push(element.type);
+        }
         createElements(element)
     });
+    singularities.forEach((element) => {
+        createButton(element);
+        clickedButtons.push(element)
+    })
 }
 
 image.onclick = () => {
-    if (image.src == (document.URL+'img/header.png')) {
+    if (image.src.includes('img/header.png')) {
         image.src = 'img/banner.png';
     }
     else {
         image.src = 'img/header.png'
     }
+}
+redefineButton.onclick = () => {
+    clickedButtons = [];
+    singularities.forEach((element) => clickedButtons.push(element));
+    let counter = 0;
+    buttonDiv.childNodes.forEach((node) => {
+        counter++;
+        if (counter >= 4) {
+            const index = singularities.indexOf(node.innerHTML.toLowerCase());
+            node.style.backgroundColor = colors[index].bg;
+            node.style.color = colors[index].txt;
+            node.style.opacity = 1;
+        }
+    });
+    reloadGlossary(redefineButton.type);
+}
+
+backToTopButton.onclick = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 }
 
 function capitalizeFirstLetter(string) {
@@ -44,6 +81,15 @@ function createElements(object) {
     const english = document.createElement('h2');
     const portuguese = document.createElement('h4');
     const description = document.createElement('p');
+    const styles = innerDiv.style;
+    if (['general', 'skills', 'untranslated'].includes(object.type)) {
+        styles.backgroundImage = 'url(img/default.png)';
+    } else {
+        styles.backgroundImage = 'url(img/' + object.type + '.png)';
+    }
+    styles.borderRadius = '8px';
+    styles.backgroundSize = 'cover';
+    styles.backgroundPosition = 'center';
 
     english.innerText = object.englishWord;
     portuguese.innerText = object.portugueseWord;
@@ -63,4 +109,51 @@ function createElements(object) {
         innerDiv.appendChild(singularity);
     }
     glossaryDiv.appendChild(innerDiv);
+}
+function reloadGlossary(origin) {
+    glossaryDiv.innerHTML = "";
+    let filteredGlossary = [];
+    if (origin == 'search') {
+        filteredGlossary = words.filter((item) => item.englishWord.toLowerCase().includes(inputSearch.value.toLowerCase()))
+    } else if (origin == 'button') {
+        filteredGlossary = words.filter((item) => clickedButtons.includes(item.type))
+    }
+    filteredGlossary.forEach((element) => { createElements(element) })
+}
+inputSearch.oninput = () => {
+    reloadGlossary(inputSearch.type);
+}
+function createButton(type) {
+    const button = document.createElement('button');
+    const index = singularities.indexOf(type);
+    button.type = "button";
+    button.id = "button";
+    button.style.backgroundColor = colors[index].bg;
+    button.style.color = colors[index].txt;
+    button.onclick = () => {
+        if (!clickedButtons.includes(type)) {
+            button.style.backgroundColor = colors[index].bg;
+            button.style.color = colors[index].txt;
+            clickedButtons.push(type);
+            button.style.opacity = 1;
+        } else {
+            button.style.backgroundColor = colors[colors.length - 1].bg;
+            button.style.color = colors[colors.length - 1].txt;
+            button.style.opacity = 0.25;
+            removeFromArray(clickedButtons, type);
+        }
+        console.log(clickedButtons);
+        reloadGlossary(button.type);
+    }
+    button.innerText = type.toUpperCase();
+    buttonDiv.appendChild(button);
+}
+
+
+function removeFromArray(array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == element) {
+            array.splice(i, 1)
+        }
+    }
 }
